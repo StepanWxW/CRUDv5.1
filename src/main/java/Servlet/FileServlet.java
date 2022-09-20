29003ed570f271f53b9a64cdace0cbb551548ae9
Stepan Polyakov <polyakovs@list.ru>
2022-09-20 11:05:58 +0700

    README.MD

--- src/main/java/Servlet/FileServlet.java
+++ src/main/java/Servlet/FileServlet.java
@@ -6,13 +6,11 @@ import controller.FileController;
 import dto.FileDTO;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.File;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/file/*")
